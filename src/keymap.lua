0d0d7fabab6cbf9b36ead5869b3ef3d8aa45c291
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2016-07-31 21:17:02 +0200

    Add ball and movement

--- src/keymap.lua
+++ src/keymap.lua
@@ -2,10 +2,22 @@ function quit()
   love.event.push("quit")
 end
 
+function moveright()
+  player.x = player.x + player.speed
+end
+
+function moveleft()
+  player.x = player.x - player.speed
+end
+
 local keymap = {
   ["escape"] = quit,
-  ["q"] = quit
+  ["q"] = quit,
+  ["right"] = moveright,
+  ["left"] = moveleft
 }
+
+-- Check the mapping and fire
 
 function dispatch(key)
   keymap[key]()
--- src/main.lua
+++ src/main.lua
@@ -4,23 +4,34 @@ keymap = require "keymap"
 
 basepaddlecenter = (love.graphics.getWidth() / 2) - 50
 
+ball = {
+  x = (love.graphics.getWidth() / 2) - 10,
+  y = (love.graphics.getHeight() / 2) - 10,
+  speed = 10,
+  img = nil
+}
+
 player = {
   x = basepaddlecenter,
   y = love.graphics.getHeight() - 40,
-  speed = 250,
+  speed = 20,
   img = nil
 }
 
 ai = {
   x = basepaddlecenter,
   y = 20,
-  speed = 250,
+  speed = 20,
   img = nil
 }
 
 function love.load()
+  -- Before you do anything, allow hold-to-move
+  love.keyboard.setKeyRepeat(true)
+
   player.img = love.graphics.newImage("images/paddle_base.png")
   ai.img = love.graphics.newImage("images/paddle_base.png")
+  ball.img = love.graphics.newImage("images/ball.png")
 end
 
 function love.update(dt)
@@ -29,6 +40,7 @@ end
 function love.draw(dt)
   love.graphics.draw(player.img, player.x, player.y)
   love.graphics.draw(ai.img, ai.x, ai.y)
+  love.graphics.draw(ball.img, ball.x, ball.y)
 end
 
 function love.keypressed(key)
